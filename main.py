33a374c6d42712682a8bdead05757b364a8429c7
GregLomason <greg.lomason@gmail.com>
2025-07-02 01:15:41 -0400

    I - Implemented asteroid spawning and grouping as per assignment

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -18,6 +20,13 @@ def main():
     
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables,)
+    
+    asteroid_field = AsteroidField()
+    
     Player.containers = (updatables, drawables)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
