707decdf888e1e481f51e23ce9ad18f1a8018ccf
GregLomason <greg.lomason@gmail.com>
2025-07-02 01:47:46 -0400

    J - Fixed CircleShape to properly initialize radius and position

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -23,7 +23,7 @@ class Player(CircleShape):
         
     def move(self, dt):
         forward = pygame.Vector2(0,1).rotate(self.rotation)
-        self.position += forward * PLAYER_TURN_SPEED * dt
+        self.position += forward * PLAYER_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
